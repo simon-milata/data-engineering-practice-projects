c980a88649db8b5609dec462cd3152ee77ab7578
Simon Milata <milata.simon@gmail.com>
2025-08-23 21:03:55 +0200

    Fix geocoding base url + other small changes

--- Section1-WeatherHolidaysPipeline/pipeline/extract.py
+++ Section1-WeatherHolidaysPipeline/pipeline/extract.py
@@ -3,19 +3,18 @@ import os
 import requests
 from urllib.parse import urlencode
 from dotenv import load_dotenv
-import pandas as pd
 
 
 load_dotenv()
-CITY = os.getenv("CITY")
-BASE_API_URL = os.getenv("BASE_API_URL")
+BASE_ARCHIVE_API_URL = os.getenv("BASE_ARCHIVE_API_URL")
+BASE_GEOCODING_API_URL = os.getenv("BASE_GEOCODING_API_URL")
 
 
 def get_city_info(city_name: str) -> tuple[float, float, str]:
     """
     Returns latitude, longitude, and timezone for a city.
     """
-    url = f"{BASE_API_URL}/search?name={city_name}&count=1&language=en&format=json"
+    url = f"{BASE_GEOCODING_API_URL}/search?name={city_name}&count=1&language=en&format=json"
     response = requests.get(url).json()
     result = response["results"][0]
     
@@ -24,23 +23,23 @@ def get_city_info(city_name: str) -> tuple[float, float, str]:
 
 def build_weather_api_url(lat: float, long: float, **kwargs) -> str:
     params = urlencode(kwargs)
-    base_url = f"{BASE_API_URL}/archive?latitude={lat}&longitude={long}&{params}"
+    base_url = f"{BASE_ARCHIVE_API_URL}/archive?latitude={lat}&longitude={long}&{params}"
 
     return base_url
 
 
-def group_weather_info(daily_weather_dict: dict) -> list[dict]:
+def group_weather_info(weather_data: dict) -> list[dict]:
     """
         Convert a dict of lists into a list of dicts, one per index.
 
         All keys in the input dict must map to lists of the same length.
     """
     grouped_data = []
-    number_of_elements = len(daily_weather_dict["time"])
+    number_of_elements = len(weather_data["time"])
 
     for i in range(number_of_elements):
         data = {}
-        for key, value in daily_weather_dict.items():
+        for key, value in weather_data.items():
             data[key] = value[i]
         grouped_data.append(data)
 
