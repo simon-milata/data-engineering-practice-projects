f5c184155a9e0a5ddb04b8b38f09c6e3e2d876a7
Simon Milata <milata.simon@gmail.com>
2025-08-23 15:46:51 +0200

    Add build_weather_api_url function with unit tests

--- Section1-WeatherHolidaysPipeline/pipeline/extract.py
+++ Section1-WeatherHolidaysPipeline/pipeline/extract.py
@@ -1,17 +1,28 @@
 import os
 
 import requests
+from urllib.parse import urlencode
 from dotenv import load_dotenv
+
 
 load_dotenv()
 CITY = os.getenv("CITY")
+BASE_API_URL = os.getenv("BASE_API_URL")
+
 
 def get_city_info(city_name: str) -> tuple[float, float, str]:
     """
     Returns latitude, longitude, and timezone for a city.
     """
-    url = f"https://geocoding-api.open-meteo.com/v1/search?name={city_name}&count=1&language=en&format=json"
+    url = f"{BASE_API_URL}/search?name={city_name}&count=1&language=en&format=json"
     response = requests.get(url).json()
     result = response["results"][0]
     
     return result["latitude"], result["longitude"], result["timezone"]
+
+
+def build_weather_api_url(lat: float, long: float, **kwargs) -> str:
+    params = urlencode(kwargs)
+    base_url = f"{BASE_API_URL}/archive?latitude={lat}&longitude={long}&{params}"
+
+    return base_url
--- Section1-WeatherHolidaysPipeline/tests/test_extract.py
+++ Section1-WeatherHolidaysPipeline/tests/test_extract.py
@@ -1,4 +1,6 @@
-from pipeline.extract import get_city_info
+from urllib.parse import urlparse, parse_qs
+
+from pipeline.extract import get_city_info, build_weather_api_url
 
 
 def test_get_city_info_success(mocker):
@@ -15,3 +17,26 @@ def test_get_city_info_success(mocker):
     assert lat == 60.16952
     assert lon == 24.93545
     assert tz == "Europe/Helsinki"
+
+
+def test_build_weather_api_url():
+    url = build_weather_api_url(
+        lat=52.52, long=13.41,
+        start="2025-08-07", end="2025-08-21",
+        hourly="temperature_2m,relative_humidity_2m,soil_moisture_7_to_28cm"
+    )
+    parsed_params = parse_qs(urlparse(url).query)
+    assert parsed_params["latitude"][0] == "52.52"
+    assert parsed_params["longitude"][0] == "13.41"
+    assert parsed_params["start"][0] == "2025-08-07"
+    assert parsed_params["end"][0] == "2025-08-21"
+    assert parsed_params["hourly"][0] == "temperature_2m,relative_humidity_2m,soil_moisture_7_to_28cm"
+
+
+def test_build_weather_api_url_no_additional_params():
+    url = build_weather_api_url(
+        lat=52.52, long=13.41
+    )
+    parsed_params = parse_qs(urlparse(url).query)
+    assert parsed_params["latitude"][0] == "52.52"
+    assert parsed_params["longitude"][0] == "13.41"
